7c6b4f6d3fed33af883b0862a7436706d26ca5d9
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-09-03 03:03:54 -0400

    removed exploit abuse for searching players

--- server/main.lua
+++ server/main.lua
@@ -803,7 +803,8 @@ RegisterNetEvent('police:server:SearchPlayer', function(playerId)
     local targetPed = GetPlayerPed(playerId)
     local playerCoords = GetEntityCoords(playerPed)
     local targetCoords = GetEntityCoords(targetPed)
-    if #(playerCoords - targetCoords) > 2.5 then return DropPlayer(src, "Attempted exploit abuse") end
+    if #(playerCoords - targetCoords) > 2.5 then return end
+        -- Removed DropPlayer(src, "Attempted exploit abuse") from before "then" above
 
     local SearchedPlayer = QBCore.Functions.GetPlayer(playerId)
     if not QBCore.Functions.GetPlayer(src) or not SearchedPlayer then return end
