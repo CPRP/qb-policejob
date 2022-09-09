e5dc8d9e8c826b39a84c97f03a51a47ee6d44aa6
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-09-09 15:33:14 -0400

    fixed tunerlaptop

--- config.lua
+++ config.lua
@@ -536,7 +536,7 @@ Config.Items = {
             authorizedJobGrades = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}
         },
         [26] = {
-            name = "tuner_laptop",
+            name = "tunerlaptop",
             price = 0,
             amount = 1,
             info = {},
