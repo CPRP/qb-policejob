f195cf1fe5d898c08d1532960c1b06cfec3a6981
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-11-17 21:34:01 -0500

    removed exploit for JailPlayers

--- server/main.lua
+++ server/main.lua
@@ -742,7 +742,8 @@ RegisterNetEvent('police:server:JailPlayer', function(playerId, time)
     local targetPed = GetPlayerPed(playerId)
     local playerCoords = GetEntityCoords(playerPed)
     local targetCoords = GetEntityCoords(targetPed)
-    if #(playerCoords - targetCoords) > 2.5 then return DropPlayer(src, "Attempted exploit abuse") end
+    if #(playerCoords - targetCoords) > 2.5 then return end
+        -- Removed DropPlayer(src, "Attempted exploit abuse") from before "then" above
 
     local Player = QBCore.Functions.GetPlayer(src)
     local OtherPlayer = QBCore.Functions.GetPlayer(playerId)
