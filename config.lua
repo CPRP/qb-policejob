cc4c6b1a6a93f5c922624601b2335182381ddded
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-12-25 07:26:52 -0500

    updated for spikestrips

--- config.lua
+++ config.lua
@@ -544,7 +544,7 @@ Config.Items = {
             slot = 26,
             authorizedJobGrades = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29}
         },
-	[27] = {
+	    [27] = {
             name = "ifaks",
             price = 0,
             amount = 10,
@@ -561,6 +561,15 @@ Config.Items = {
                 serie = "",
             },
             type = "weapon",
+            slot = 28,
+            authorizedJobGrades = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29}
+        },
+        [28] = {
+            name = "spikestrip",
+            price = 0,
+            amount = 20,
+            info = {},
+            type = "item",
             slot = 28,
             authorizedJobGrades = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29}
         },
